d08e3d4f7092d3ffb3956cd49a58d3c5598c5f92
Christopher Antila <christopher@antila.ca>
2015-11-10 22:56:23 -0500

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -45,13 +45,24 @@ class PyTest(Command):
         raise SystemExit(errno)
 
 
+_LONG_DESCRIPTION = '''
+It’s simple: Fujian accepts Python code in the request body of a PUT request, executes the code, then returns the result.
+
+The server is named after Fujian (福建) Province of the People’s Republic of China. The intention is to use it with our “lychee” software (“litchi” package on PyPI). Lychees are a fruit that grow in southern China. Fujian is a province in southern China. That’s about it.
+
+Do note that this application opens the door to a wide range of security issues, most of which we don’t plan to address. Fujian is intended for use on localhost only. Opening it up to the public Internet is a tremendously bad idea!
+
+We will migrate to PyPy3.
+'''
+
+
 setup(
     name = 'Fujian',
     version = fujian.__version__,
     packages = ['fujian', 'tests'],
 
     install_requires = ['tornado>=4'],
-    # TODO: add pytest in here somehow
+    tests_require = ['pytest>2.7'],
 
     cmdclass = {'test': PyTest},
 
@@ -59,8 +70,8 @@ setup(
     author = 'Christopher Antila',
     author_email = 'christopher@antila.ca',
     description = 'An HTTP server that executes Python code.',
+    long_description = _LONG_DESCRIPTION,
     license = 'AGPLv3+',
-    keywords = 'idk',
+    keywords = 'tornado, http server, execute python',
     url = 'jameson.adjectivenoun.ca/ncoda/fujian',
-    # TODO: long_description, download_url, classifiers, etc.
 )
